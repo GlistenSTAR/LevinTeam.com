10a452a91e8ca6ffb8f5469a18583a31e8608448
GlistenSTAR <glistenstar00@gmail.com>
2021-07-22 06:36:44 +0200

    change the Navbar modification

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment,  useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 import Logo from "./Logo";
-
+import img from '../../img/logos/Logo Bottom Layer.png'
+import img_changed from '../../img/logos/white_logo.png'
 
 const Navbar = ({ auth: { isAuthenticated }, logout }) => {
+  const [changeNavbar, setNavbar] = useState(false);
+  
+  useEffect(() => {
+    window.addEventListener('scroll', listenScrollEvent)
+  });
+
+  const listenScrollEvent = e => {
+    if (window.scrollY > 100) {
+      setNavbar(true)
+    } else {
+      setNavbar(false)
+    }
+  }
+
   const authLinks = (
     <ul className="navbar-nav ml-auto">
-      <li className="nav-item">
-        <Link className="nav-link" to="/profiles">Developers</Link>
-      </li>
-      <li>
-        <Link className="nav-link" to="/posts">Posts</Link>
-      </li>
-      <li>
-        <Link className="nav-link" to="/dashboard">
-          <i className="fas fa-user" />{' '}
-          <span className="hide-sm">Dashboard</span>
-        </Link>
-      </li>
       <li>
         <a onClick={logout} href="#!">
           <i className="fas fa-sign-out-alt" />{' '}
@@ -34,19 +37,19 @@ const Navbar = ({ auth: { isAuthenticated }, logout }) => {
     <div>
       <ul className="first_row">
         <li>
-          <Link className="nav-link simplo_font" to="/profiles">
+          <Link className="nav-link simplo_font" to="/#">
             <i className="fas fa-phone-square"></i>
             516.316.0158
           </Link>
         </li>
         <li>
-          <Link className="nav-link simplo_font" to="/register">
+          <Link className="nav-link simplo_font" to="/#">
             <i className="fas fa-mail-bulk"></i>
             INFO@LEVINTEAM.COM   
           </Link>
         </li>
         <li>
-          <Link className="nav-link" to="/login">
+          <Link className="nav-link" to="/#">
             AVAILABLE 24 Hours/Day, 7 Days/Week!
           </Link>
         </li>
@@ -58,33 +61,33 @@ const Navbar = ({ auth: { isAuthenticated }, logout }) => {
       </ul>  
       <ul className="second_row">
         <li>
-          <Link className="nav-link" to="/profiles">Search Properties</Link>
+          <Link className="nav-link" to="/#">Search Properties</Link>
         </li>
         <li>
-          <Link className="nav-link" to="/register">
+          <Link className="nav-link" to="/#">
             Sell
             <i className="fas fa-angle-down"></i>
           </Link>
         </li>
         <li>
-          <Link className="nav-link" to="/login">
+          <Link className="nav-link" to="/#">
             Buy
             <i className="fas fa-angle-down"></i>
           </Link>
         </li>
         <li>
-          <Link className="nav-link" to="/login">
+          <Link className="nav-link" to="/#">
             About LHT
             <i className="fas fa-angle-down"></i>
           </Link>
         </li>
         <li>
-          <Link className="nav-link" to="/login">
+          <Link className="nav-link" to="/#">
             Join The Team
           </Link>
         </li>
         <li>
-          <Link className="nav-link" to="/login">
+          <Link className="nav-link" to="/#">
             Connect
           </Link>
         </li>
@@ -93,12 +96,24 @@ const Navbar = ({ auth: { isAuthenticated }, logout }) => {
   );
 
   return (
-    <nav className="navbar">
-      <Link className="nav-link" to="/">
-        <Logo />
-      </Link>
-      <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
-    </nav>
+    <>
+      {!changeNavbar?
+      (
+        <nav className="navbar">
+          <Link className="nav-link" to="/">
+            <Logo img = {img} width="292px" height="147px"/>
+          </Link>
+          <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+        </nav>
+      ):(
+        <nav className="navbar_changed">
+          <Link className="nav-link" to="/">
+            <Logo img={img_changed} width="163px" height="82px"/>
+          </Link>
+          <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+        </nav>
+      )}
+    </>
   );
 };
 
