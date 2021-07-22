5bc3d28fbc3aaf9a2aad7c5afe34bf6505957aab
GlistenSTAR <glistenstar00@gmail.com>
2021-07-22 06:35:22 +0200

    footer page

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="text-white p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} DevConnector
+    </footer>
+  )
+}
