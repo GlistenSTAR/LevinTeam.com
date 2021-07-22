444563700d15dccfa23aa354ad9367f4ca14f701
GlistenSTAR <glistenstar00@gmail.com>
2021-07-22 06:36:22 +0200

    change the navbar following mouse scroll

--- client/src/components/layout/Logo.js
+++ client/src/components/layout/Logo.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import img from '../../img/logos/Logo Bottom Layer.png'
 
-export default function Logo() {
+export default function Logo({img, width, height}) {
   return (
     <img 
       src={img} 
       className="item_image" 
       // width="292px" 
-      height="147px"
-      style={{
-        width:'292px',
-      }}
+      height={height}
+      width={width}
     />
   )
 }
